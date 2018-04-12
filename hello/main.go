f91bedec8a44f7be796cb0752e42cba854197586
Paulo Costa <paulombcosta@gmail.com>
2018-04-11 23:18:56 -0300

    Simple request

--- hello/main.go
+++ hello/main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-lambda-go/events"
 )
 
 type Response struct {
 	Message string `json:"message"`
 }
 
-func Handler() (Response, error) {
-	return Response{
-		Message: "Go Serverless v1.0! Your function executed successfully!",
-	}, nil
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Println("Received body ", request.Body)
+	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
 }
 
 func main() {
